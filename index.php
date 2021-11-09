2357c97f7ce0cb98e3f968fde7307fa34c3d5e61
Arifur Rahman Arif <dragonwarrior052@gmail.com>
2021-11-09 19:04:33 +0600

    a

--- index.php
+++ index.php
@@ -1,29 +1,31 @@
 <?php
 
-try {
-    $user = 'skyler@chrisscottwellness.com';
-    $password = 'qVfn#5d6M$fNXB2';
-    $customerKey = 'j43qd27kp89a';
+// try {
+//     $user = 'skyler@chrisscottwellness.com';
+//     $password = 'qVfn#5d6M$fNXB2';
+//     $customerKey = 'j43qd27kp89a';
 
-    $wsdl = 'https://webservice.kareo.com/services/soap/2.1/KareoServices.svc?singleWsdl';
-    $client = new SoapClient($wsdl);
+//     $wsdl = 'https://webservice.kareo.com/services/soap/2.1/KareoServices.svc?singleWsdl';
+//     $client = new SoapClient($wsdl);
 
-    $request = array(
-        'RequestHeader' => array('User' => $user, 'Password' => $password, 'CustomerKey' => $customerKey),
-        'Filter'        => array(
-            // 'FromLastModifiedDate' => '3/4/2021',
-            'FromCreatedDate' => '10/5/2021'
-            // 'CreatedDate' => '10/5/2021 8:20:22 AM'
-        )
-    );
+//     $request = array(
+//         'RequestHeader' => array('User' => $user, 'Password' => $password, 'CustomerKey' => $customerKey),
+//         'Filter'        => array(
+//             // 'FromLastModifiedDate' => '3/4/2021',
+//             'FromCreatedDate' => '11/1/2021'
+//             // 'CreatedDate' => '10/5/2021 8:20:22 AM'
+//         )
+//     );
 
-    $params = array('request' => $request);
-    $response = $client->GetPatients($params)->GetPatientsResult;
+//     $params = array('request' => $request);
+//     $response = $client->GetPatients($params)->GetPatientsResult;
 
-    echo '<pre>';
-    print_r($response);
-    echo '</pre>';
+//     echo '<pre>';
+//     print_r($response);
+//     echo '</pre>';
 
-} catch (Exception $err) {
-    print "Error: " . $err->getMessage();
-}
+// } catch (Exception $err) {
+//     print "Error: " . $err->getMessage();
+// }
+
+echo "Hello world";
